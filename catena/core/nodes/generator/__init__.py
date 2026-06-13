263fde41a34b0847bbb287b839a47c4b2364809b
nate-maxwell <natedmaxwell@gmail.com>
2026-06-12 21:57:15 -0700

    adjusted node colors

--- catena/core/nodes/generator/__init__.py
+++ catena/core/nodes/generator/__init__.py
@@ -1,3 +1,3 @@
 from PySide6TK import QtGui
 
-IMAGE_NODE_COLOR = QtGui.QColor(255, 25, 225)
+IMAGE_NODE_COLOR = QtGui.QColor(64, 25, 128)
--- catena/core/nodes/math/__init__.py
+++ catena/core/nodes/math/__init__.py
@@ -1,3 +1,3 @@
 from PySide6TK import QtGui
 
-IMAGE_NODE_COLOR = QtGui.QColor(0, 255, 255)
+IMAGE_NODE_COLOR = QtGui.QColor(0, 128, 128)
