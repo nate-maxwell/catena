ae2fa32dd8a2eb65bcc1b51b259a41e6198a165c
natedmaxwell <natedmaxwell@gmail.com>
2026-06-16 16:31:52 -0700

    import standardization

--- catena/main.py
+++ catena/main.py
@@ -1,8 +1,8 @@
 import sys
 
-from PySide6 import QtCore
-from PySide6 import QtGui
-from PySide6 import QtWidgets
+from PySide6TK import QtCore
+from PySide6TK import QtGui
+from PySide6TK import QtWidgets
 
 from catena.core import resources
 from catena.core.client import CatenaEditor
