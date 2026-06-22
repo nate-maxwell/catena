ba29d23a83971039fba71ef62c08550711bd1b6b
natedmaxwell <natedmaxwell@gmail.com>
2026-06-22 14:47:04 -0700

    removed unused preferences menu field

--- catena/preferences/general.py
+++ catena/preferences/general.py
@@ -10,10 +10,6 @@ class GeneralPreferencesMenu(PreferenceTopicMenu):
 
         self.topic_prefs = self.prefs.general_preferences
 
-        self.auto_save_interval = QtWrappers.LabeledSpinBox("Auto Save Interval")
-        self.auto_save_interval.set_value(self.topic_prefs.auto_save_interval)
-        self.add_widget(self.auto_save_interval)
-
         self.texture_resolution = QtWrappers.LabeledSpinBox("Texture Resolution")
         self.texture_resolution.set_value(self.topic_prefs.texture_resolution)
         self.add_widget(self.texture_resolution)
@@ -21,5 +17,4 @@ class GeneralPreferencesMenu(PreferenceTopicMenu):
         self.add_stretch()
 
     def sync_settings(self) -> None:
-        self.topic_prefs.auto_save_interval = self.auto_save_interval.value()
         self.topic_prefs.texture_resolution = self.texture_resolution.value()
